8a42cef27f472922a1695b2ca3cdaac4b7f6e2b7
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2023-09-18 04:08:42 +0530

    Update ColorPalette.js

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -36,7 +36,7 @@ const ColorPalette = () => {
 
   return (
     <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-      <h1 style={{ fontSize: "4rem", marginBottom: "1.5rem", marginTop: "0.5rem" }}>Color Palette</h1>
+      <h1 style={{ fontSize: "4rem", marginBottom: "1.5rem" }}>Color Palette</h1>
       {colors.map((color) => (
         <div
           key={color.color}
