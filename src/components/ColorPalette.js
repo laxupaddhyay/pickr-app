760e784e3c15aed7a3082f45ca4e9bd8daafc021
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2023-09-18 04:29:28 +0530

    Update ColorPalette.js

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -24,10 +24,10 @@ const ColorPalette = () => {
     const brightness = (bgColor.match(/(\d+)/g) || []).reduce((a, b) => a + parseInt(b), 0) / 300;
 
     if (brightness < 0.1) {
-      return "text-white";
+      return "white";
     }
 
-    return brightness > 0.5 ? "text-black" : "text-white";
+    return brightness > 0.5 ? "black" : "white";
   };
 
   const copyToClipboard = (code) => {
@@ -35,17 +35,25 @@ const ColorPalette = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="mb-4 text-center">
-        <h1 className="text-4xl">Color Palette</h1>
-      </div>
+    <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <h1 style={{ fontSize: "4rem", marginBottom: "1.5rem" }}>Color Palette</h1>
       {colors.map((color) => (
         <div
           key={color.color}
-          style={{ backgroundColor: color.color }}
-          className={`w-full h-1/5 flex items-center justify-center cursor-pointer text-1.5rem ${getTextColor(color.color)}`}
+          style={{
+            backgroundColor: color.color,
+            width: "100%",
+            height: "13%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "pointer",
+            fontSize: "1.5rem",
+            color: getTextColor(color.color),
+          }}
           onClick={() => copyToClipboard(color.code)}
         >
+          {color.code}
         </div>
       ))}
     </div>
