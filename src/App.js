2f5aebe0eac3581b01547426f0e7ef8fd6ef9a93
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2023-09-18 03:58:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Colors from "./components/Colors";
 import Gradient from "./components/Gradient";
 import ColorShades from "./components/ColorShades";
+import ColorPalette from "./components/ColorPalette";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Colors />} />
             <Route path="/gradient" element={<Gradient />} />
             <Route path="/color-shades" element={<ColorShades />} />
+            <Route path="/color-palette" element={<ColorPalette />} />
           </Routes>
         </main>
       </BrowserRouter>
