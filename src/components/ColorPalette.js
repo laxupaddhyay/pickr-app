c75d0cddac8fcf5f657b1bc8bd3e5e1c330b8d48
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2023-09-20 17:39:07 +0530

    Update ColorPalette.js

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -1,66 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import colorScheme from 'color-scheme';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const ColorPalette = () => {
+function ColorPalette() {
   const [colors, setColors] = useState([]);
 
   useEffect(() => {
-    const newColors = [];
-
-    while (newColors.length < 5) {
-      const hue = Math.round(Math.random() * 360);
-      const saturation = Math.round(Math.random() * 100);
-      const lightness = Math.round(Math.random() * 100);
-      const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-
-      if (!newColors.some((c) => c.color === color)) {
-        newColors.push({ color, code: color });
-      }
-    }
-
-    setColors(newColors);
+    generateColors();
   }, []);
 
-  const getTextColor = (bgColor) => {
-    const brightness = (bgColor.match(/(\d+)/g) || []).reduce((a, b) => a + parseInt(b), 0) / 300;
+  const generateColors = () => {
+    const scheme = new colorScheme();
+    scheme.from_hue(Math.floor(Math.random() * 360))
+      .scheme('triade')
+      .variation('default');
 
-    if (brightness < 0.1) {
-      return "white";
-    }
-
-    return brightness > 0.5 ? "black" : "white";
+    const colorList = scheme.colors().slice(0, 5); // Always start with the first 5 colors
+    const capitalizedColors = colorList.map(color => color.toUpperCase());
+    setColors(capitalizedColors.map(color => `#${color}`)); // Add "#" to each color code
   };
 
-  const copyToClipboard = (code) => {
-    navigator.clipboard.writeText(code);
+  const copyToClipboard = (color) => {
+    navigator.clipboard.writeText(color);
+    toast.success('Copied ' + color, {
+      position: "top-center",
+      autoClose: 1500,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      theme: "dark", // Enable dark mode
+    });
   };
 
   return (
-    <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundColor: "white"}}>
-       <div className="title flex flex-col items-center">
-          <h1 className="xs:text-5xl md:text-6xl font-bold">Color Palette</h1>
-          <span className="py-4 md:text-2xl sm:text-xl w-9/10 text-center xs:text-[12px] text-gray-900">Enhance your pages</span>
-        </div>
-      {colors.map((color) => (
-        <div
-          key={color.color}
-          style={{
-            backgroundColor: color.color,
-            width: "100%",
-            height: "13%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            cursor: "pointer",
-            fontSize: "1.5rem",
-            color: getTextColor(color.color),
-          }}
-          onClick={() => copyToClipboard(color.code)}
-        >
-          {color.code}
-        </div>
-      ))}
+    <div className="p-4 text-center">
+      <h2 className="text-4xl font-bold">Colour Palette</h2>
+      <span className="text-lg text-gray-600 block mb-4">Enhance your webpages</span>
+      <div className="color-container flex flex-wrap justify-center md:justify-start -mx-2">
+        {colors.map((color, index) => (
+          <div key={index} className="color-box m-2 w-full sm:w-full md:w-1/3 lg:w-1/5">
+            <div
+              className="color cursor-pointer"
+              style={{ backgroundColor: color, height: '100px' }}
+              onClick={() => copyToClipboard(color)}
+            >
+              <p
+                className="text-gray-700 mt-2 select-all cursor-pointer text-left px-2"
+                onClick={() => copyToClipboard(color)}
+              >
+                {color}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button
+        className="mt-8 py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white rounded-full shadow-md"
+        onClick={generateColors}
+      >
+        Generate
+      </button>
+      <ToastContainer />
     </div>
   );
-};
+}
 
 export default ColorPalette;
+                    
