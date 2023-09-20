11b5c3d0fe036f2e6a9928e494352875b3050b13
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2023-09-20 18:20:05 +0530

    Update ColorPalette.js

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -12,8 +12,8 @@ function ColorPalette() {
 
   const generateColors = () => {
     const darkColors = chroma.scale(['#000', '#333']).colors(2); // Two shades of dark colors
-    const lightColors = chroma.scale(['#f0f', '#ccf']).colors(3); // Three shades of light colors
-    const accentColors = chroma.scale(['#f90', '#f06']).colors(0); // Four shades of an accent color
+    const lightColors = chroma.scale(['#f0f', '#ccf']).colors(2); // Three shades of light colors
+    const accentColors = chroma.scale(['#f90', '#f06']).colors(1); // Four shades of an accent color
 
     const colorPalette = [...darkColors, ...lightColors, ...accentColors];
 
