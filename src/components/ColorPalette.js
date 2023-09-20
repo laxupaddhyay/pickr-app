38631e96336adf24c81641cdae5eb7ee391d6bf0
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2023-09-20 17:49:59 +0530

    Update ColorPalette.js

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import colorScheme from 'color-scheme';
+import chroma from 'chroma-js';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,14 +11,14 @@ function ColorPalette() {
   }, []);
 
   const generateColors = () => {
-    const scheme = new colorScheme();
-    scheme.from_hue(Math.floor(Math.random() * 360))
-      .scheme('triade')
-      .variation('default');
+    const darkColors = chroma.scale(['#000', '#333']).colors(2); // Two shades of dark colors
+    const lightColors = chroma.scale(['#f0f', '#ccf']).colors(3); // Three shades of light colors
+    const accentColors = chroma.scale(['#f90', '#f06']).colors(4); // Four shades of an accent color
 
-    const colorList = scheme.colors().slice(0, 5); // Always start with the first 5 colors
-    const capitalizedColors = colorList.map(color => color.toUpperCase());
-    setColors(capitalizedColors.map(color => `#${color}`)); // Add "#" to each color code
+    const colorPalette = [...darkColors, ...lightColors, ...accentColors];
+
+    const capitalizedColors = colorPalette.map(color => color.toUpperCase());
+    setColors(capitalizedColors);
   };
 
   const copyToClipboard = (color) => {
@@ -37,16 +37,16 @@ function ColorPalette() {
     <div className="p-4 text-center">
       <h2 className="text-4xl font-bold">Colour Palette</h2>
       <span className="text-lg text-gray-600 block mb-4">Enhance your webpages</span>
-      <div className="color-container flex flex-wrap justify-center md:justify-start">
+      <div className="color-container flex flex-wrap justify-center md:justify-start -mx-2">
         {colors.map((color, index) => (
           <div key={index} className="color-box m-2 w-full sm:w-full md:w-1/3 lg:w-1/5">
             <div
-              className="color relative cursor-pointer"
+              className="color cursor-pointer"
               style={{ backgroundColor: color, height: '100px' }}
               onClick={() => copyToClipboard(color)}
             >
               <p
-                className="text-gray-700 mt-2 select-all cursor-pointer text-left absolute left-0 right-0 bottom-2 px-2"
+                className="text-gray-700 mt-2 select-all cursor-pointer text-left px-2"
                 onClick={() => copyToClipboard(color)}
               >
                 {color}
