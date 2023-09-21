44f9262660a2b99fad2cdde052568a02616d22c6
LAKSHYA UPADDHYAY <95523785+laxupaddhyay@users.noreply.github.com>
2023-09-21 15:26:49 +0530

    ColorPalette.js updated

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import colorScheme from 'color-scheme';
-import { ToastContainer, toast } from 'react-toastify';
+import { GrClipboard } from "react-icons/gr";
+import { ToastContainer, toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function ColorPalette() {
@@ -10,7 +11,6 @@ function ColorPalette() {
     generateColors();
   }, []);
 
-  // Generate a color palette using the color-scheme library
   const generateColors = () => {
     const scheme = new colorScheme();
     scheme.from_hue(Math.floor(Math.random() * 360))
@@ -18,52 +18,76 @@ function ColorPalette() {
       .variation('default');
 
     const colorList = scheme.colors().slice(0, 5);
-    const capitalizedColors = colorList.map(color => color.toUpperCase());
-    const colorCodesWithHash = capitalizedColors.map(color => `#${color}`);
+    const capitalizedColors = colorList.map((color) => color.toUpperCase());
+    const colorCodesWithHash = capitalizedColors.map((color) => `#${color}`);
     setColors(colorCodesWithHash);
   };
 
-  // Copy a color code to the clipboard and show a toast notification
   const copyToClipboard = (color) => {
     navigator.clipboard.writeText(color);
-    toast.success(`Copied ${color}`, {
-      position: "top-center",
-      autoClose: 1000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: false,
-      theme: "dark",
-    });
+    toast.success(`Copied ${color}`)
   };
 
+  const getContrastTextColor = (bgColor) => {
+    const r = parseInt(bgColor.slice(1, 3), 16);
+    const g = parseInt(bgColor.slice(3, 5), 16);
+    const b = parseInt(bgColor.slice(5, 7), 16);
+
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+    return brightness >= 128 ? 'black' : 'white';
+  };
+
+  const handleKeyPress = useCallback(
+    (event) => {
+      if (event.key === ' ' || event.key === 'Spacebar') {
+        event.preventDefault();
+        generateColors();
+      }
+    },
+    [generateColors]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
   return (
-    <div className="mt-2 text-center">
-      <h1 className="text-5xl font-bold mt-10">Color Palette</h1>
-      <span className="text-md text-gray-600 block mb-4">Enhance your webpages</span>
-      <div className="color-container flex flex-wrap justify-center md:justify-start">
+    <div className="text-center">
+      <h1 className="text-6xl font-bold mt-10">Color Palette</h1>
+      <span className="text-md text-gray-900 block mb-8">Press <span className='font-bold'>Spacebar</span> to generate new palettes</span>
+      <div className="color-container flex flex-wrap justify-center items-center md:justify-start">
         {colors.map((color, index) => (
           <div key={index} className="w-screen md:w-1/5">
             <div
-              className="cursor-pointer h-[100px] lg:h-[55vh]"
-              style={{ backgroundColor: color }}
+              className={`flex flex-col justify-center items-center h-[100px] md:h-[68vh]`}
+              style={{ backgroundColor: color, color:getContrastTextColor(color) }}
             >
               <p
-                className="text-gray-700 cursor-pointer text-left items-center px-2"
-                onClick={() => copyToClipboard(color)}
+                className="flex z-99 font-bold text-lg mb-4"
               >
                 {color}
               </p>
+              <GrClipboard className="cursor-pointer text-black" onClick={() => copyToClipboard(color)}/>
             </div>
           </div>
         ))}
       </div>
-      <button
-        className="mt-8 py-2 px-4 bg-black hover:bg-gray-600 text-white rounded-lg shadow-md"
-        onClick={generateColors}
-      >
-        Generate
-      </button>
-      <ToastContainer />
+      
+      <ToastContainer
+      position="top-center"
+      autoClose={1000}
+      transition={Zoom}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      draggable={false}
+      theme="dark"
+      />
     </div>
   );
 }
